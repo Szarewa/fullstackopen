87f03e4dc90c5b07358b0ce55ce03735250bff63
Szarewa <sfarisizarewa@gmail.com>
2023-11-23 23:00:51 +0100

    unicafe step5 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,24 +2,34 @@ import './App.css'
 import { useState } from "react"
 
 const Button = ({ handleClick, feedback }) => {
-  //const [handleClick, feedback] = props
+  //console.log(feedback)
+  const myButton = () => feedback.map((feedback, i) => {
+    return(
+      <button key={i} onClick={handleClick} >{feedback.text}</button>
+    )
+  })
+
   return(
-    <button onClick={handleClick}>{feedback}</button>
+    <div>
+      {myButton()}
+    </div>
   )
 }
 
-const StatisticLine = () => {
-  return (
-    <div></div>
+const StatisticsLine = ({ text, value }) => {
+  return(
+    <div>
+      <p>{text}: {value}</p>
+    </div>
   )
 }
 
-const Statistics = ({ feedbacks }) =>{
-  const good = feedbacks[0]
-  const neutral = feedbacks[1]
-  const bad = feedbacks[2]
+const Statistics = ({ feedbackValue }) => {
+  const good = feedbackValue[0]
+  const neutral = feedbackValue[1]
+  const bad = feedbackValue[2]
 
-  const total = feedbacks.reduce((acc, fdbck) => acc + fdbck, 0)
+  const total = feedbackValue.reduce((acc, fdbck) => acc + fdbck, 0)
   const average = (total/3).toFixed(2)
   let percentageGood
 
@@ -33,18 +43,17 @@ const Statistics = ({ feedbacks }) =>{
   if(total === 0){
     return 'No feedback given yet...'
   }
-  
+
   return(
     <div>
-      <h3>Number of feedbacks</h3>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <hr/>
-      <h3>Feedback statistics</h3>
-      <p>Total feedback: {total}</p>
-      <p>Average feedback: {average}</p>
-      <p>Positive feedback: {percentageGood}</p>
+      <h3>Feedbacks</h3>
+      <StatisticsLine text='Good' value={good} />
+      <StatisticsLine text='Neutral' value={neutral} />
+      <StatisticsLine text='Bad' value={bad} />
+      <h3>Feedback Statistics</h3>
+      <StatisticsLine text='Total' value={total} />
+      <StatisticsLine text='Average' value={average} />
+      <StatisticsLine text='Percentage Good' value={percentageGood} />
     </div>
   )
 }
@@ -54,16 +63,40 @@ const App = (props) => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const feedbacks = [good, neutral, bad]
+  const feedback = [
+    {text: 'Good', value: good},
+    {text: 'Neutral', value: neutral},
+    {text: 'Bad', value: bad}
+  ]
+
+  const feedbackValue = [good, neutral, bad]
+
+const handleClick = () => {
+  const setValue = () => {
+    if (event.target.textContent === 'Good') {
+      setGood(good => good +1)
+    }
+    
+
+    if (event.target.textContent === 'Neutral') {
+      setNeutral(neutral => neutral +1)
+    }
+    
+
+    if (event.target.textContent === 'Bad') {
+      setBad(bad => bad +1)
+    }
+    
+  }
+  return setValue()
+}
 
   return (
     <div className="unicafe">
-      <h3>Give your feedback</h3>
-      <Button handleClick={() => setGood(good + 1)} feedback='Good' />
-      <Button handleClick={() =>setNeutral(neutral + 1)} feedback='Neutral' />
-      <Button handleClick={() =>setBad(bad + 1)} feedback='Bad' />
+      <Button handleClick={handleClick} feedback={feedback} />
       <hr/>
-      <Statistics feedbacks={feedbacks} />
+      <Statistics feedbackValue={feedbackValue} />
+      <hr/>
     </div>
   )
 }
