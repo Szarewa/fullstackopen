f98b4bc79bd1cf337a90cc06d7c3de8581353f08
Szarewa <sfarisizarewa@gmail.com>
2023-12-01 15:48:01 +0100

    phonebook step3 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,25 @@
 import { useState } from 'react'
 
+const PersonData = (props) => {
+  return(
+    <div>
+      {props.persons.map((person, i) => (
+        <p key={i}>{person.name}: {person.number}</p>
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   //handling the form submit event
   const handleSubmit = (e) => {
@@ -19,18 +36,25 @@ const App = () => {
       alert(`${newName} is already added...`)
     }
     else {
-      //adding a new object to the state with the newName property
-      const updatedPersonsArray = [...persons, {name: newName}]
+      //adding a new object to the state with the newName and newPhoneNumber property
+      const updatedPersonsArray = [...persons, {name: newName, number: newPhone}]
       setPersons(updatedPersonsArray)
-      //console.log(updatedPersonsArray)
+      //alert(`${newName} with number: ${newPhone} added to phonebook...`)
     }
 
     setNewName('')
+    setNewPhone('')
   }
 
+  //getting the new name
   const handleInputChange = (e) => {
     const newData = e.target.value
     setNewName(newData)
+  }
+  //getting the new phone
+  const handlePhoneInput= (e) => {
+    const newPhoneNumber = e.target.value
+    setNewPhone(newPhoneNumber)
   }
 
   return (
@@ -41,11 +65,14 @@ const App = () => {
           name: <input onChange={handleInputChange} />
         </div>
         <div>
+          Phone: <input onChange={handlePhoneInput} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <PersonData persons={persons} />
     </div>
   )
 }
