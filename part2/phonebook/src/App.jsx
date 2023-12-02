b0175f224c23172ca7222bd888c48a4d30dc7cce
Szarewa <sfarisizarewa@gmail.com>
2023-12-02 20:27:25 +0100

    phonebook assignment completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,8 @@
 import { useState } from 'react'
-
-const PersonData = (props) => {
-  return(
-    <div>
-      {props.persons.map((person, i) => (
-        <p key={i}>{person.name}: {person.number}</p>
-      ))}
-    </div>
-  )
-}
+import Search from './components/Search'
+import Forms from './components/Forms'
+import PersonData from './components/PersonData'
+import './App.css'
 
 const App = () => {
   
@@ -20,59 +14,54 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [searchParam, setSearchParam] = useState('')
 
-  //handling the form submit event
   const handleSubmit = (e) => {
-    //preventing the default behavior
     e.preventDefault()
     
-    //mapping through the persons array
     const person = persons.map(person => person.name)
-    //checking if the newName is added already
     const nameInPerson = person.includes(newName)
 
-    //alerting the user if name was added
     if(nameInPerson){
       alert(`${newName} is already added...`)
     }
     else {
-      //adding a new object to the state with the newName and newPhoneNumber property
-      const updatedPersonsArray = [...persons, {name: newName, number: newPhone}]
+      const updatedPersonsArray = [...persons, {name: newName, number: newPhone, id: persons.length + 1}]
       setPersons(updatedPersonsArray)
-      //alert(`${newName} with number: ${newPhone} added to phonebook...`)
+      alert('Contact added...')
     }
 
     setNewName('')
     setNewPhone('')
   }
 
-  //getting the new name
   const handleInputChange = (e) => {
     const newData = e.target.value
     setNewName(newData)
   }
-  //getting the new phone
   const handlePhoneInput= (e) => {
     const newPhoneNumber = e.target.value
     setNewPhone(newPhoneNumber)
   }
 
+  const handleSearch = (e) => {
+    setSearchParam(e.target.value)
+  }
+
+  const filteredName = searchParam 
+    ? persons.filter(person => person.name.toLowerCase().includes(searchParam.toLowerCase()) ) : persons
+
   return (
-    <div>
+    <div className='phonebook'>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input onChange={handleInputChange} />
-        </div>
-        <div>
-          Phone: <input onChange={handlePhoneInput} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <PersonData persons={persons} />
+      <Search handleSearch={handleSearch} />
+
+      <hr/>
+      <h3>Register User:</h3>
+      <Forms handleSubmit={handleSubmit} handleInputChange={handleInputChange} handlePhoneInput={handlePhoneInput} />
+
+      <h3>Numbers</h3>
+      <PersonData persons={filteredName} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Forms.jsx
@@ -0,0 +1,17 @@
+import './Forms.css'
+
+const Forms = ({ handleSubmit, handleInputChange, handlePhoneInput }) => {
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <p>name: <input onChange={handleInputChange} /></p>
+                <p>Phone: <input onChange={handlePhoneInput} /></p>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Forms;
